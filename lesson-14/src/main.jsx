b25025ad7b4d63bf4a047af5de73e2256a24a391
Oleksandr <alex24061126@gmail.com>
2025-09-14 09:43:02 +0300

    2

--- lesson-14/src/main.jsx
+++ lesson-14/src/main.jsx
@@ -1,8 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import './reset.css'
-import App from './App'
+import './reset.css'
+import { RouterProvider } from 'react-router'
+// import App from './App'
+import router from './router'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  // <App />
 )
---
+++ lesson-14/src/router/frontRoutes.js
@@ -0,0 +1,32 @@
+export const frontRoutes = {
+  pages: {
+    home: '/',
+    patients: {
+      base: 'patients',
+      edit: 'edit/:id?',
+      details: 'details/:id'
+    },
+    doctors: {
+      base: 'doctors',
+      edit: 'edit/:id?',
+    },
+    appointments: {
+      base: 'appointments',
+      edit: 'edit/:id?',
+    },
+  },
+  navigate: {
+    patients: {
+      list: '/patients',
+      edit: (id) => `/patients/edit/${id}`,
+      create: '/patients/edit',
+      details: (id) => `/patients/details/${id}`
+    },
+    doctors: {
+      list: '/doctors',
+      edit: (id) => `/doctors/edit/${id}`,
+      create: '/doctors/edit',
+      details: (id) => `/doctors/details/${id}`
+    }
+  },
+}
